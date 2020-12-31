6df2edc36c68b2f40a8a89bc65d4d47c17f64e9d
Srimanta Bagchi <srimantabagchi@gmail.com>
2020-12-30 22:37:26 -0500

    Create UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    AudioCache audioCache = new AudioCache();
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                flex: 1,
+                child: FlatButton(
+                  color: Colors.red,
+                  onPressed: () => audioCache.play('note1.wav'),
+                  child: null,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: FlatButton(
+                  color: Colors.orange,
+                  onPressed: () => audioCache.play('note2.wav'),
+                  child: null,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: FlatButton(
+                  color: Colors.yellow,
+                  onPressed: () => audioCache.play('note3.wav'),
+                  child: null,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: FlatButton(
+                  color: Colors.green,
+                  onPressed: () => audioCache.play('note4.wav'),
+                  child: null,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: FlatButton(
+                  color: Colors.teal,
+                  onPressed: () => audioCache.play('note5.wav'),
+                  child: null,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: FlatButton(
+                  color: Colors.blue,
+                  onPressed: () => audioCache.play('note6.wav'),
+                  child: null,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: FlatButton(
+                  color: Colors.purple,
+                  onPressed: () => audioCache.play('note7.wav'),
+                  child: null,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
